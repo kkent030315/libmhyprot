1eb233f5f4437b5846ff84f43aaecb814c775fe5
kento <37926134+kkent030315@users.noreply.github.com>
2020-10-20 08:03:09 +0900

    code cleanup

--- src/file_utils.cpp
+++ src/file_utils.cpp
@@ -29,15 +29,15 @@
 // create file from memory
 //
 bool file_utils::create_file_from_buffer(
-	const std::string_view file_path, void* buffer, size_t size
+	const std::string_view file_path, void* buffer, const size_t size
 )
 {
     std::ofstream stream(
         file_path.data(),
         std::ios_base::out | std::ios_base::binary
-    );
+	);
 
-	if (!stream.write((char*)buffer, size))
+	if (!stream.write(reinterpret_cast<char*>(buffer), size))
 	{
 		stream.close();
 		return false;
